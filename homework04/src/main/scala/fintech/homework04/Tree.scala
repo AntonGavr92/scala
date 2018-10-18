1db5183b3b1a6aa735fd8d836b3d83b14eab51a6
Anton <sith_1@mail.ru>
2018-10-18 09:29:17 +0500

    Homework04.

---
+++ homework04/src/main/scala/fintech/homework04/Tree.scala
@@ -0,0 +1,77 @@
+package fintech.homework04
+
+sealed trait Tree[+A]
+
+case class Leaf[A](value: A) extends Tree[A]
+
+case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+object Tree {
+  // реализовать функцию fold
+
+  val MaxCountLeafInBranch = 2
+
+  def fold[A, B](t: Tree[A])(f: A => B)(g: (B, B) => B): B = {
+    @tailrec
+    def foldTree(folded: Option[B], treeSeq: Queue[Tree[A]])(f: A => B)(g: (B, B) => B): B = {
+      if (treeSeq.isEmpty) {
+        folded.get
+      } else {
+        val (tree, newQueue) = treeSeq.dequeue
+        tree match {
+          case leaf: Leaf[A] => foldTree(fold(leaf.value, folded), newQueue)(f)(g)
+          case branch: Branch[A] => foldTree(folded, newQueue ++ List(branch.right, branch.left))(f)(g)
+        }
+      }
+    }
+
+    def fold(a: A, b: Option[B]): Option[B] = {
+      b.map(value => g(value, f(a))).orElse(Option(f(a)))
+    }
+
+    foldTree(Option.empty, Queue(t))(f)(g)
+  }
+
+  // реализовать следующие функции через fold
+
+  def size[A](t: Tree[A]): Int = {
+    fold(t)(_ => 1)((x, y) => x + y)
+  }
+
+  def max(t: Tree[Int]): Int = {
+    fold(t)(identity)((x, y) => math.max(x, y))
+  }
+
+  def depth[A](t: Tree[A]): Int = {
+    size(t) / MaxCountLeafInBranch + 1
+  }
+
+  //здесь возможно придется прибегнуть к насильному указанию типа: Leaf(a): Tree[A]
+  def map[A, B](t: Tree[A])(f: A => B): Tree[B] = {
+    t match {
+      case leaf: Leaf[A] => new Leaf[B](f(leaf.value))
+      case branch: Branch[A] => new Branch[B](map(branch.left)(f), map(branch.right)(f))
+    }
+  }
+
+  def equals[B](left: Tree[B], right: Tree[B]): Boolean = {
+
+    val DefaultState = true
+
+    @tailrec
+    def equalsTree(curState: Boolean, queueTuple: Queue[(Tree[B], Tree[B])]): Boolean = {
+      if (!curState || queueTuple.isEmpty)
+        curState
+      else {
+        val ((left, right), newQueue) = queueTuple.dequeue
+        (left, right) match {
+          case (left: Leaf[B], right: Leaf[B]) => left == right
+          case (leftB: Branch[B], rightB: Branch[B]) => equalsTree(curState, newQueue ++ List((leftB.left, rightB.left), (leftB.right, rightB.right)))
+          case _ => false
+        }
+      }
+    }
+
+    equalsTree(DefaultState, Queue((left, right)))
+  }
+}
---
+++ homework04/src/test/scala/fintech/homework04/TreeSpec.scala
@@ -0,0 +1,37 @@
+package fintech.homework04
+
+class TreeSpec extends FlatSpec with Matchers {
+
+  val tree = new Branch[Int](
+    new Branch[Int](new Leaf[Int](3), new Leaf[Int](4)),
+    new Leaf[Int](1)
+  )
+
+  val doubleTree = new Branch[Double](
+    new Branch[Double](new Leaf[Double](3.0), new Leaf[Double](4.0)),
+    new Leaf[Double](1.0)
+  )
+
+  "Tree" should "be fold correct" in {
+    Tree.fold(tree)(x => x.toDouble)((x, y) => x + y) shouldBe 8.0
+  }
+
+  "Method size of Tree" should "be work correct" in {
+    Tree.size(tree) shouldBe 3
+  }
+
+  "Method max of Tree" should "be get maximal value of leaf in tree" in {
+    Tree.max(tree) shouldBe 4
+  }
+
+  "Method depth of Tree" should "be work correct" in {
+    Tree.depth(tree) shouldBe 2
+  }
+
+  "Method map of Tree" should "be work correct" in {
+    Tree.equals(Tree.map(tree)(x => x.toDouble), doubleTree) shouldBe true
+    Tree.size(Tree.map(tree)(x => x.toDouble)) shouldBe Tree.size(tree)
+    Tree.size(Tree.map(tree)(x => x.toDouble)) shouldBe Tree.size(tree)
+    Tree.depth(Tree.map(tree)(x => x.toDouble)) shouldBe Tree.depth(tree)
+  }
+}
