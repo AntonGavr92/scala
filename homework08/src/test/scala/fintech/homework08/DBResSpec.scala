47f4cfdf0997c7c6ff7ce2e9b78812625c26a730
Anton <sith_1@mail.ru>
2018-11-15 00:31:31 +0500

    Homework08.

--- homework08/src/test/scala/fintech/homework08/DBResSpec.scala
+++ homework08/src/test/scala/fintech/homework08/DBResSpec.scala
@@ -7,16 +7,14 @@ class DBResSpec extends FlatSpec with Matchers {
   val uri = "jdbc:h2:~/dbres"
 
   "Method map of DBRes" should "be work correct" in {
-    DBRes.update("DROP TABLE people", List.empty)
-      .map(_ => DBRes.select("SELECT name FROM people", List.empty)(x => x.getString("count"))).execute(uri) shouldBe List()
+    DBRes.update("CREATE TABLE people(name VARCHAR(256), birthday DATE)", List.empty)
+      .map(_ => DBRes.select("SELECT name FROM people", List.empty)(x => x.getString("count"))).isInstanceOf[DBRes[List[String]]] shouldBe true
   }
 
   "Method flatMap of DBRes" should "be work correct" in {
-    DBRes.update("DROP TABLE people", List.empty)
-      .flatMap(_ => DBRes.update("CREATE TABLE people(name VARCHAR(256), birthday DATE)", List.empty)
+    DBRes.update("CREATE TABLE people(name VARCHAR(256), birthday DATE)", List.empty)
         .flatMap(_ => DBRes.select("SELECT name FROM people", List.empty)(x => x.getString("count"))
-        )
-      ).execute(uri) shouldBe List()
+        ).execute(uri) shouldBe List()
   }
 
 }
